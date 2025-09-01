4a07a342ebb747b52552abad10197e27785fb2f2
Anahi <anahi7776@gmail.com>
2025-08-31 20:20:07 -0400

    fix username api call, still in development

--- src/bodyInicio.jsx
+++ src/bodyInicio.jsx
@@ -3,7 +3,7 @@ import cflove from "./assets/imgs/cflove.png"
 
 function BodyInicio(){
     return (
-        <div className= "justify-items-center">
+        <div className= "col-span-full justifu-center content-center">
             <img src={cflove} alt="cflove" className = 'h-100 w-100 justify-items-center'/>
             <FrasesAnimadas></FrasesAnimadas>
         </div>
--- src/codeforces/codeforcesView.jsx
+++ src/codeforces/codeforcesView.jsx
@@ -4,12 +4,11 @@ import SelectionMenuCF from "./selectionCodeforces"
 import { useState } from "react";
 function CodeforcesView(){
     const [divView, setDivView] = useState("div1");
-    const [username, setUsername] = useState("");
     return(
         <div className="grid grid-cols-1">
-            <div className="justify-items-end m-4"><SearchBar title="username" setUsername={setUsername}></SearchBar></div>
+            <div className="flex justify-end m-4"><SearchBar title="username" ></SearchBar></div>
             <SelectionMenuCF setDivView = {setDivView}></SelectionMenuCF>
-            <ContestsGrid divView = {divView} username={username}/>
+            <ContestsGrid divView = {divView}/>
         </div>
     )
 }
